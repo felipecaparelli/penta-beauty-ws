cf7f5befed1439c643698f3c9c9ff36399426c2a
Felipe Caparelli <felipe.caparelli@fisglobal.com>
2017-07-02 01:18:05 -0300

    inserindo ouath

--- app.js
+++ app.js
@@ -16,11 +16,53 @@
 
 'use strict';
 
+const credentials = {
+  client: {
+    id: '<client-id>',
+    secret: '<client-secret>'
+  },
+  auth: {
+    tokenHost: 'https://api.oauth.com'
+  }
+};
+
+const tokenConfig = {
+  username: 'usuario',
+  password: '123456' 
+};
+
 var express    = require('express'),
   app          = express(),
+  bodyParser   = require('body-parser'),
+  mongoose 	   = require('mongoose'),
+  Schema 	   = mongoose.Schema,
+  MongoClient  = require('mongodb').MongoClient,
+  jwt 		   = require('jsonwebtoken'),
+  oauth2 	   = require('simple-oauth2').create(credentials),
   watson       = require('watson-developer-cloud'),
   extend       = require('util')._extend,
   i18n         = require('i18next');
+  
+  
+var ProductSchema = new Schema({
+  name: {
+    type: String,
+    Required: 'Por favor, preencha o nome do produto'
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'ongoing', 'completed']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Products', ProductSchema);
 
 //i18n settings
 require('./config/i18n')(app);
@@ -50,9 +92,29 @@ app.post('/api/profile', function(req, res, next) {
   });
 });
 
+app.post('/api/login', function(req, res, next) {
+	// Save the access token
+	oauth2.ownerPassword.getToken(tokenConfig, (error, result) => {
+	  if (error) {
+		return console.log('Access Token Error', error.message);
+	  }
+
+	  const token = oauth2.accessToken.create(result);
+	});
+});
+
+
+app.get('/api/shop', function (req, res) {
+   //item
+	//console.log(req.body);
+	res.end(req.body);
+})
+
 // error-handler settings
 require('./config/error-handler')(app);
 
 var port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
 app.listen(port);
+
 console.log('listening at:', port);
